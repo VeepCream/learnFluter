75a9026e3b1a510488c6c40feaf9f8dc950a89b2
veep-cream <dev.mobile.veep@gmail.com>
2020-12-07 01:09:45 +0700

    Set theme for the application via theme property

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     title: 'My App',
+    theme: ThemeData(primarySwatch: Colors.green),
     home: Scaffold(
       appBar: AppBar(
         title: Text('Counter'),
