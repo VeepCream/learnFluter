6ce51d5fd43921a59ce0cfd747ede1b93f0cd2c7
veep-cream <dev.mobile.veep@gmail.com>
2020-12-07 14:00:43 +0700

    create first Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My App',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Counter'),
-        ),
-        body: Text('hello, flutter'),
+      home: CounterArea(),
+    );
+  }
+}
+
+class CounterArea extends StatefulWidget {
+  CounterArea({Key key}) : super(key: key);
+
+  @override
+  _CounterAreaState createState() => _CounterAreaState();
+}
+
+class _CounterAreaState extends State<CounterArea> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
       ),
+      body: Text('hello, flutter'),
     );
   }
 }
