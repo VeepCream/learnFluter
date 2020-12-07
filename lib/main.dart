e6f168b48be8d1757df340cabc645ab6f95a67a6
veep-cream <dev.mobile.veep@gmail.com>
2020-12-07 13:54:20 +0700

    create first Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: 'My App',
-    theme: ThemeData(primarySwatch: Colors.green),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Counter'),
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
 
-  runApp(app);
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Counter'),
+        ),
+        body: Text('hello, flutter'),
+      ),
+    );
+  }
 }
