3db017f913bbe948a33d90be520ed9e2f045f750
Veerapat Preechapant <dev.mobile.veep@gmail.com>
2021-02-11 17:27:05 +0700

    Make you press select and make a call

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'contact.dart';
 
 void main() {
@@ -43,7 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: Image.asset(contact.imageSource),
               title: Text(contact.name),
               subtitle: Text(contact.phoneNumber),
-              onTap: () {},
+              onTap: () {
+                launch('tel:${contact.phoneNumber}');
+              },
             );
           },
         ));
