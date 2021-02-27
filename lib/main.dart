57ec904a6d943274deb638f8b5216661424ecd17
Veerapat Preechapant <dev.mobile.veep@gmail.com>
2021-02-27 23:03:57 +0700

    Use the Widget New Post Page with the Navigator push command

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn/pages/new_post_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -39,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (BuildContext context) {
-                    return Text('');
+                    return NewsPostPage();
                   }));
                 })
           ],
