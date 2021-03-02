7acf8e72405579cc1f7b7c3efa262d96fc52466c
Veerapat Preechapant <dev.mobile.veep@gmail.com>
2021-03-02 13:06:25 +0700

    Create a function to update the information in the Provider

--- lib/provider/post_provider.dart
+++ lib/provider/post_provider.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/foundation.dart';
 
 class PostProvider with ChangeNotifier {
-  List<String> _post = [];
+  List<String> _posts = [];
 
-  List<String> get posts => _post;
+  List<String> get posts => _posts;
+
+  addNewPost(String post) {
+    _posts.add(post);
+
+    notifyListeners();
+  }
 }
