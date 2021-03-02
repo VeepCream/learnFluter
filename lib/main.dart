67ecfc9c8acb7aabb46b1800d8f7647df4a642e7
Veerapat Preechapant <dev.mobile.veep@gmail.com>
2021-03-02 13:13:05 +0700

    Adjust Provider settings to a format of Fat Arrow Function

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (BuildContext context) {
-            return PostProvider();
-          },
-        )
+        ChangeNotifierProvider(create: (BuildContext context) => PostProvider())
       ],
       child: MaterialApp(
         title: 'Nextflow Personal Post',
